eef12b661b9e93e4e91fef95619aeb5ce5da81e1
houchunan <houchunan@heiyan.com>
2020-01-04 11:07:11 +0800

    增加MD5、日期转换工具类

--- nvwa-service/src/main/java/com/codeagles/impl/UserServiceImpl.java
+++ nvwa-service/src/main/java/com/codeagles/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.codeagles.impl;
 
+import com.codeagles.bo.UserBO;
 import com.codeagles.mapper.UsersMapper;
 import com.codeagles.pojo.Users;
 import com.codeagles.service.UserSerivce;
+import com.codeagles.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -21,6 +23,8 @@ public class UserServiceImpl implements UserSerivce {
     @Autowired
     private UsersMapper usersMapper;
 
+    public static final String USER_FACE = "http://img.codeagles.com/5m1AgeRLf3.jpg";
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public boolean queryByUserName(String name) {
@@ -30,5 +34,25 @@ public class UserServiceImpl implements UserSerivce {
         Users users = usersMapper.selectOneByExample(example);
 
         return users == null?false:true;
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBO userBO) {
+
+        Users user = new Users();
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //设置默认昵称
+        user.setNickname(userBO.getUsername());
+        //设置默认头像
+        user.setFace(USER_FACE);
+        user.setBirthday();
+        return null;
     }
 }
