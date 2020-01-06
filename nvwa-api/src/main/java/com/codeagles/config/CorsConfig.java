47b0a19d88bfb0120459fee6444eb2434638dfe0
codeagles <hcn0521@outlook.com>
2020-01-06 23:23:28 +0800

    设置前后端跨域请求cors

---
+++ nvwa-api/src/main/java/com/codeagles/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package com.codeagles.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Codeagles
+ * Date: 2020/1/6
+ * Time: 10:49 PM
+ * <p>
+ * Description:
+ */
+//@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //1.添加cors配置信息
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+
+        //设置是否发送cookie信息
+        configuration.setAllowCredentials(true);
+        //设置允许请求的方式
+        configuration.addAllowedMethod("*");
+        //设置允许的header
+        configuration.addAllowedHeader("*");
+        //2. 为url添加映射路径
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", configuration);
+        //3. 返回重新定义好的corsSource
+        return new CorsFilter(corsSource);
+    }
+}
